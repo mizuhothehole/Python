dacd4054a4f49cb00e5d538f5d960eca04724e7d
Shin <goldeyeqin@gmail.com>
2017-05-14 13:27:52 +0900

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,25 @@
+textbrfore="Hello"
+keys="3rfcf3ertv4hvgvh5vehrtehvvvjthvrvthtrvhtyvj6yt4rfhevmym"
+print textbrfore;
+
+class App(object):
+
+   def __init__(self):
+      self
+   def encryptionFunction(self,textContent,key):
+      self
+      code = ""
+      for iterating_var in textContent:
+         code+=(iterating_var+ key)
+      return code
+   def recryptionFunction(self,textContent,key):
+      self
+      code = ""
+      code = textContent.replace(key,'')
+      return code
+
+app = App()
+code = app. encryptionFunction(textbrfore, keys)
+print code
+recode = app. recryptionFunction(code, keys)
+print recode
